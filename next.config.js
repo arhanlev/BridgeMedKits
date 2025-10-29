9d1f1e598bfbbec5b63327a7d4420db0e22f1e5d
arhanlev <arhan.levaku@icloud.com>
2025-10-29 19:09:10 -0400

    Comment out output setting in next.config.js
    Comment out the output setting for static exports

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',  // Enable static exports
+  //output: 'export',  // Enable static exports
   images: {
     unoptimized: true, // Required for static export
   },
